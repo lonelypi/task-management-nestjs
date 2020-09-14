7b244f740056434a92b1acdeeccc0e648ce1632b
Pedro Ivo <pedroivodefaria@gmail.com>
2020-09-14 00:26:13 -0300

    First commit of the task management app.

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface'
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'taskmanagement',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+}
